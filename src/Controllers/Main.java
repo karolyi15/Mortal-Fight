cdf8e7f6c4dd63e780434905477107d2505f1210
karoly15 <karolyi15@hotmail.com>
2020-07-20 16:39:14 -0600

    Initial Commit

---
+++ src/Controllers/Main.java
@@ -0,0 +1,76 @@
+package Controllers;
+
+import Controllers.Views.TitleScene_Controller;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        this.primaryStage=primaryStage;
+        this.primaryStage.setTitle("Mortal Fight");
+
+        this.initRootLayout();
+        this.showTitleScene();
+
+    }
+
+    private void initRootLayout(){
+
+        try{
+
+            //Load Fxml File
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("Views/RootLayout.fxml"));
+            this.rootLayout = (BorderPane) loader.load();
+
+            //Create Scene
+            Scene rootLayoutScene = new Scene(this.rootLayout);
+
+            //Set Root Layout to Stage
+            this.primaryStage.setScene(rootLayoutScene);
+            this.primaryStage.show();
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void showTitleScene(){
+
+        try{
+
+            //Load Fxml File
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("Views/TitleScene_UI.fxml"));
+            AnchorPane titleScene = loader.load();
+
+            //Set Title Scene Controller
+            TitleScene_Controller controller = loader.getController();
+            controller.setMainApp(this);
+
+            //Set Scene to Root Layout
+            this.rootLayout.setCenter(titleScene);
+
+        }catch (IOException e){
+
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/Controllers/Views/TitleScene_Controller.java
@@ -0,0 +1,33 @@
+package Controllers.Views;
+
+import Controllers.Main;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class TitleScene_Controller {
+
+    private Main mainApp;
+
+    @FXML
+    private Button start_Button;
+    @FXML
+    private Button about_Button;
+
+
+    public void setMainApp(Main mainApp){
+
+        this.mainApp=mainApp;
+    }
+
+    @FXML
+    private void onHandleStart(){
+
+        System.out.println("Start Game");
+    }
+
+    @FXML
+    private void onHandleAbout(){
+
+        System.out.println("Game Info");
+    }
+}
