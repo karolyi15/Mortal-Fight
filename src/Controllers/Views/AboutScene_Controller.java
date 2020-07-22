11f2b3413736fc7e82defc26c40314b06315c1e5
karoly15 <karolyi15@hotmail.com>
2020-07-21 20:52:15 -0600

    Scene Controllers

--- src/Controllers/Views/AboutScene_Controller.java
+++ src/Controllers/Views/AboutScene_Controller.java
@@ -1,12 +1,40 @@
 package Controllers.Views;
 
 import Controllers.Main;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 
 public class AboutScene_Controller {
 
+    //********************************************************************************************************//
+    //********************************************* CLASS FIELDS *********************************************//
+
     private Main mainApp;
 
+    @FXML
+    private Button return_Button;
+    @FXML
+    private Button Information_Button;
+    @FXML
+    private Button Commands_Button;
+    @FXML
+    private  Button Types_Button;
+    @FXML
+    private TextArea content_TextArea;
+
+    //********************************************************************************************************//
+    //******************************************** CLASS METHODS *********************************************//
+
+    //*** Setters & Getters ***
     public void setMainApp(Main mainApp) {
         this.mainApp = mainApp;
+    }
+
+    //*** Button Handlers ***
+    @FXML
+    private void onHandleReturn(){
+
+        this.mainApp.showTitleScene();
     }
 }
--- src/Controllers/Views/MenuScene_Controller.java
+++ src/Controllers/Views/MenuScene_Controller.java
@@ -1,12 +1,38 @@
 package Controllers.Views;
 
 import Controllers.Main;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class MenuScene_Controller {
 
+    //********************************************************************************************************//
+    //********************************************* CLASS FIELDS *********************************************//
+
     private Main mainApp;
 
+    @FXML
+    private Button selectUser_Button;
+    @FXML
+    private Button createUser_Button;
+
+    //********************************************************************************************************//
+    //******************************************** CLASS METHODS *********************************************//
+
+    //*** Setters & Getters ***
     public void setMainApp(Main mainApp) {
         this.mainApp = mainApp;
     }
+
+    //*** Button Handlers ***
+    @FXML
+    private void onHandleSelectUser(){
+
+        this.mainApp.showGameScene();
+    }
+    @FXML
+    private void onHandleCreateUser(){
+
+    }
+
 }
